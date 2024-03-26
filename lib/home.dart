18e07d96c7a43290cde5af96e3472cbfae79aa96
LeandroClaytonPivovarsky <leandro123clayton@gmail.com>
2024-03-26 11:55:09 -0300

    Realizados algumas alterações feitas pelo professor

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -29,13 +31,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Jokenpo(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Jokenpo extends StatefulWidget {
+  const Jokenpo({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -49,85 +51,171 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Jokenpo> createState() => _JokenpoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _JokenpoState extends State<Jokenpo> {
+  final _imgPadrao = Image(image: AssetImage('assets/images/padrao.png'));
 
+  var _imgApp = Image(image: AssetImage('assets/images/padrao.png'));
+  var _imgAppMaquina = Image(image: AssetImage('assets/images/padrao.png'));
+  String _mensagem = 'Resultado';
+  int numVitorias = 0;
+  int numDerrotas = 0;
+  int numEmpates = 0;
+
+  void _joga(String opcaoJogador) {
+    final lista_opcoes = ['pedra', 'papel', 'tesoura'];
+    final opcaoApp = lista_opcoes[Random().nextInt(3)];
+
+    setState(() {
+      _imgApp = Image(image: AssetImage('assets/images/$opcaoJogador.png'));
+      _imgAppMaquina = Image(image: AssetImage('assets/images/$opcaoApp.png'));
+      _mensagem = _resultado(opcaoJogador, opcaoApp);
+    });
+  }
+
+  String _resultado(String opcaoJogador, String opcaoApp) {
+    final mensagem;
+
+    if ((opcaoJogador == 'pedra' && opcaoApp == 'tesoura') ||
+        (opcaoJogador == 'papel' && opcaoApp == 'pedra') ||
+        (opcaoJogador == 'tesoura' && opcaoApp == 'papel')) {
+      numVitorias++;
+      return 'Jogador ganhou!!';
+    } else if (opcaoApp == opcaoJogador) {
+      numEmpates++;
+      return 'Empate!!!';
+    } else {
+      numDerrotas++;
+      return "A máquina ganhou!";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: const Text("Jokenpo"),
-        ),
-        body:const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text("Jokenpo"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(bottom: 30),
+            child: textHome(_mensagem),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Padding(
-                padding: EdgeInsets.all(30),
-                child:  Text("Resultado",
-                textScaler: TextScaler.linear(3),) ,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 30, bottom: 30, left: 15, right: 15),
-                child:Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text("Sua escolha")
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Image(
-                            image: AssetImage('assets/images/padrao.png'),
-                        ),
-                      ),
-                    ]),
-                    Column(children: [
-                      Text(
-                        "X",
-                        textScaler: TextScaler.linear(3),)
-                    ],),
-
-                    Column(children: [
-                      Text("Escolha da máquina"),
-                      Align(
-                      alignment: Alignment.centerRight,
-                      child: Image(
-                        image: AssetImage('assets/images/padrao.png'))
-                    )
-                    ],)
-                  ],),
+              Column(children: [
+                SizedBox(
+                  width: 120,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: textHome("Sua escolha"),
+                  ),
                 ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                Align(alignment: Alignment.center, child: _imgApp),
+              ]),
+              Column(
                 children: [
-                  SizedBox(
-                    width: 80,
-                    child: Image(
-                      image: AssetImage('assets/images/pedra.png')),
-                    
-                  ),
-                  SizedBox(
-                    width: 80,
-                    child: Image(
-                      image: AssetImage('assets/images/papel.png')),
-                    
-                  ),
-                  SizedBox(
-                    width: 80,
-                    child: Image(
-                      image: AssetImage('assets/images/tesoura.png')),
-                    
+                  Text(
+                    "X",
+                    textScaler: TextScaler.linear(3),
                   )
-                ])
+                ],
+              ),
+              Column(
+                children: [
+                  textHome("Escolha da máquina"),
+                  Align(alignment: Alignment.centerRight, child: _imgAppMaquina)
+                ],
+              ),
+            ],
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            choisePlayerDetector('pedra',
+                image: Image.asset(
+                  'assets/images/pedra.png',
+                  width: 80,
+                )),
+            choisePlayerDetector('papel',
+                image: Image.asset(
+                  'assets/images/papel.png',
+                  width: 80,
+                )),
+            choisePlayerDetector('tesoura',
+                image: Image.asset(
+                  'assets/images/tesoura.png',
+                  width: 80,
+                )),
+          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              counterWrapCircle('Vitórias', numVitorias.toString()),
+              counterWrapCircle('Empate', numEmpates.toString()),
+              counterWrapCircle('Derrota', numDerrotas.toString()),
             ],
           )
-      );
-    
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            numVitorias = 0;
+            numEmpates = 0;
+            numDerrotas = 0;
+            _imgApp = _imgPadrao;
+            _imgAppMaquina = _imgPadrao;
+          });
+        },
+        child: const Icon(Icons.settings_backup_restore_sharp),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+    );
+  }
+
+  Text textHome(String text) {
+    return Text(
+      text,
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+    );
+  }
+
+  GestureDetector choisePlayerDetector(String PlayerOpp, {Image? image}) {
+    return GestureDetector(
+      onTap: () => _joga(PlayerOpp),
+      child: image,
+    );
+  }
+
+  SizedBox counterWrapCircle(String textTop, String textBottom) {
+    return SizedBox(
+        height: 90,
+        width: 90,
+        child: ClipOval(
+          child: Material(
+            color: Theme.of(context).colorScheme.primary,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                textTop,
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                textBottom,
+              )
+            ]),
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:jokenpo/home.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-    debugShowMaterialGrid: false,
-    theme: ThemeData(brightness:  Brightness.light,),
-    home: const MyHomePage(title: "leandraoApp")));
+  runApp(MaterialApp(
+      debugShowMaterialGrid: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan)
+      ),
+      home: const Jokenpo(title: "leandraoApp")));
 }
-
-
